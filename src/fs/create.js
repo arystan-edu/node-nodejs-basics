98bb58841c75da4d5fb3e5b1ca1212b2defb7d4a
arystan-edu <arystan.edu@gmail.com>
2022-06-06 00:54:16 +0600

    Init Commit

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,27 @@
+import fs from 'fs';
+
 export const create = async () => {
-    // Write your code here 
+    // create.js - implement function that creates new file fresh.txt 
+    // with content I am fresh and young inside of the files folder 
+    // (if file already exists Error with message FS operation failed must be thrown)
+
+    var filePath = './src/fs/files/fresh.txt';
+    var content = 'I am fresh and young';
+
+    try {
+        if (fs.existsSync(filePath)) {
+            throw new Error('FS operation failed')
+        }
+    } 
+    catch(err) {
+        console.error(err)
+    }
+    finally {
+        try {
+            await fs.promises.writeFile(filePath, content); // need to be in an async function
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
 };
